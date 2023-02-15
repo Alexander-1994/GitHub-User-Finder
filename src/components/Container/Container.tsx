61b437b7af43dbf76b59bc2f0754a8c213118081
Alexander <kisliy.doma@gmail.com>
2023-02-15 15:46:53 +0800

    start

---
+++ src/components/Container/Container.tsx
@@ -0,0 +1,11 @@
+import styles from './Container.module.scss';
+
+interface ContainerProps { 
+  
+}
+
+export const Container = ({ }: ContainerProps) => (
+	<div className={styles.container}>
+		Container Component
+	</div>
+);
---
+++ src/components/TheSwitcher/TheSwitcher.tsx
@@ -0,0 +1,9 @@
+import styles from './TheSwitcher.module.scss';
+
+interface TheSwitcherProps { }
+
+export const TheSwitcher = ({ }: TheSwitcherProps) => (
+	<div className={styles.theSwitcher}>
+		TheSwitcher Component
+	</div>
+);
